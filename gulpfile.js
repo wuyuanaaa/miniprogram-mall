16efb2897010f6ccedb73bacdee900b644a8a679
wuyuanaaa <dao332508553@163.com>
2019-03-21 15:41:50 +0800

    更新 构建工具 首页等

--- gulpfile.js
+++ gulpfile.js
@@ -2,8 +2,12 @@ var gulp = require('gulp');
 var less = require('gulp-less');
 var rename = require('gulp-rename');
 var plumber = require('gulp-plumber');
+var postcss = require('gulp-postcss');
+var pxtounits = require('postcss-px2units');  // 解决出错时停止后续执行
+var sequence = require('gulp-sequence');  // 按顺序同步执行多个任务
 
-var lessPath = ['./miniprogram/**/**/*.less','./miniprogram/**/*.less']
+var lessPath = ['./miniprogram/**/**/*.less','./miniprogram/**/*.less'];
+var wxssPath = ['./miniprogram/**/**/*.wxss','./miniprogram/**/*.wxss'];
 
 gulp.task('less', function () {
   return gulp.src(lessPath)
@@ -15,7 +19,16 @@ gulp.task('less', function () {
     .pipe(gulp.dest('./miniprogram'));
 });
 
-// 静态服务器
+gulp.task('wxss', function () {
+  return gulp.src(wxssPath)
+    .pipe(postcss([pxtounits()]))
+    .pipe(gulp.dest('./miniprogram'));
+});
+
+gulp.task('fix',function (cb) {
+  sequence('less','wxss')(cb);
+});
+
 gulp.task('default', function () {
-  gulp.watch(lessPath, ['less']);
+  gulp.watch(lessPath, ['fix']);
 });
--- miniprogram/pages/index/index.js
+++ miniprogram/pages/index/index.js
@@ -125,6 +125,134 @@ Page({
             "url": ""
           }
         ]
+      },
+      {
+        "classify": "computer",
+        "banner": {
+          "info": "小米笔记本",
+          "url": "",
+          "src": "https://i8.mifile.cn/v1/a1/0f84ed79-2446-2f93-f72e-456798ab5749!720x440.webp"
+        },
+        "list": [
+          {
+            "title": "15.6\" i5 8G MX110",
+            "dec": "全面均衡的国民轻薄本",
+            "currentPrice": "4399",
+            "price": "4599",
+            "priceDec": "",
+            "src": "https://i8.mifile.cn/v1/a1/48664b4c-07bb-f276-7fbb-a79a7ddd4a7e!360x360.webp",
+            "url": ""
+          },
+          {
+            "title": "13.3\" i7 MX150 灰色",
+            "dec": "四核i7处理器 高性能独显",
+            "currentPrice": "5999",
+            "price": "",
+            "priceDec": "",
+            "src": "https://i8.mifile.cn/v1/a1/780ebdf2-669a-923b-752d-8aecb392a08b!360x360.webp",
+            "url": ""
+          },
+          {
+            "title": "15.6\" i7 16G MX150",
+            "dec": "高性能独显 第八代处理器",
+            "currentPrice": "6999",
+            "price": "",
+            "priceDec": "",
+            "src": "https://i8.mifile.cn/v1/a1/93aa5dda-ae56-3dac-d19a-8fd390189216!360x360.webp",
+            "url": ""
+          },
+          {
+            "title": "13.3\" i7 MX150 银色",
+            "dec": "四核i7处理器 高性能独显",
+            "currentPrice": "5999",
+            "price": "",
+            "priceDec": "",
+            "src": "https://i8.mifile.cn/v1/a1/82df8302-8625-d544-8b12-cd37c8b1dd0b!360x360.webp",
+            "url": ""
+          },
+          {
+            "title": "游戏本i7 GTX1060",
+            "dec": "性能怪兽 信仰级游戏体验",
+            "currentPrice": "8599",
+            "price": "8999",
+            "priceDec": "",
+            "src": "https://i8.mifile.cn/v1/a1/0f5dc643-183c-5bce-8de3-0f0829e9c3d5!360x360.webp",
+            "url": ""
+          },
+          {
+            "title": "12.5\" m3 128G 银色",
+            "dec": "轻薄长续航 超窄边框",
+            "currentPrice": "3599",
+            "price": "",
+            "priceDec": "",
+            "src": "https://i8.mifile.cn/v1/a1/28bf863f-1c2d-52b8-a2e5-186dfcbaad1e!360x360.webp",
+            "url": ""
+          }
+        ]
+      },
+      {
+        "classify": "tv",
+        "banner": {
+          "info": "小米电视",
+          "url": "",
+          "src": "https://i8.mifile.cn/v1/a1/f5599110-2fe2-2011-00cf-4d78eb2d3a9c!720x440.webp"
+        },
+        "list": [
+          {
+            "title": "小米电视4C 43英寸",
+            "dec": "FHD全高清屏，钢琴烤漆",
+            "currentPrice": "1499",
+            "price": "",
+            "priceDec": "",
+            "src": "https://i8.mifile.cn/v1/a1/3961de40-63f1-16cc-6ced-2c034f1f5e43!360x360.webp",
+            "url": ""
+          },
+          {
+            "title": "小米电视4A 65英寸",
+            "dec": "4K HDR，人工智能语音系统",
+            "currentPrice": "3199",
+            "price": "3699",
+            "priceDec": "",
+            "src": "https://i8.mifile.cn/v1/a1/b509d62f-a10a-6559-94f4-852b401815fb!360x360.webp",
+            "url": ""
+          },
+          {
+            "title": "小米电视4A 32英寸",
+            "dec": "人工智能系统，高清液晶屏",
+            "currentPrice": "799",
+            "price": "899",
+            "priceDec": "",
+            "src": "https://i8.mifile.cn/v1/a1/d342ccbf-e3d6-2dcc-47a3-18656f28a0cc!360x360.webp",
+            "url": ""
+          },
+          {
+            "title": "小米电视4A 49英寸",
+            "dec": "人工智能语音，FHD全高清屏",
+            "currentPrice": "1699",
+            "price": "1799",
+            "priceDec": "",
+            "src": "https://i8.mifile.cn/v1/a1/c46f28b5-636e-ffcc-c0cb-2347bd2c4890!360x360.webp",
+            "url": ""
+          },
+          {
+            "title": "小米电视4C 40英寸",
+            "dec": "FHD全高清屏，钢琴烤漆",
+            "currentPrice": "1199",
+            "price": "1299",
+            "priceDec": "",
+            "src": "https://cdn.cnbj0.fds.api.mi-img.com/b2c-mimall-media/4aa77a674400f1b836b33ba1d54a8d23.jpg?thumb=1&w=360&h=360",
+            "url": ""
+          },
+          {
+            "title": "小米电视4C 50英寸",
+            "dec": "4K HDR，钢琴烤漆",
+            "currentPrice": "1899",
+            "price": "2099",
+            "priceDec": "",
+            "src": "https://i8.mifile.cn/v1/a1/c547c9f7-1703-a6ae-79e0-4e27912595f8!360x360.webp",
+            "url": ""
+          }
+        ]
       }
     ],
   },
@@ -143,7 +271,7 @@ Page({
   },
 
   onShow: function () {
-    
+
   },
 
   // 页面分享设置
